ca9b490981e75683e23c79ae4ed22a536aff4226
zxcmixka <pikopavel@gmail.com>
2025-04-23 14:17:34 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { HeaderComponent } from "./componets/HeaderComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
